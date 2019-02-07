74da9d30bb8a69e8da1dd06fd84f50d62433a11e
sheikabdullah5 <47408771+sheikabdullah5@users.noreply.github.com>
2019-02-07 01:16:27 -0800

    Update vowelrconsonant.py

--- vowelrconsonant.py
+++ vowelrconsonant.py
@@ -1,8 +1,8 @@
 sheik=['a','e','i','o','u','A','E','I','O','U']
 s=raw_input()
-if(s in list):
-	print('vowel')
-elif(s!=list):
-	print('consonant')
+if(s in sheik):
+	print('Vowel')
+elif(s!=sheik):
+	print('Consonant')
 else:
 	print('invalid')
